44833105983a0b06240a5fef1e3d01d050e86d63
Lance Wang <lancewang@google.com>
2025-12-17 21:33:22 -0800

    Code update
    PiperOrigin-RevId: 846059994

--- tunix/models/qwen3/model.py
+++ tunix/models/qwen3/model.py
@@ -75,7 +75,7 @@ class ShardingConfig:
         ffw_weight_fd=('tp', fsdp),
         rms_norm_weight=('tp',),
         act_btd=('fsdp', None, None if is_sampling else 'tp'),
-        act_btf=('fsdp', None, 'tp'),
+        act_btf=('fsdp', None, None),
         act_btnh=('fsdp', None, 'tp', None),
         exp_weight_cdf=('fsdp', None, 'tp'),
         exp_weight_cfd=('fsdp', 'tp', None),
